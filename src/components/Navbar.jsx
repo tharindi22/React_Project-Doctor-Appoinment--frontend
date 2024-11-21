2828f1eab246318bf9bc7fb77e20f9284a054a77
tharindi22 <ttharindiattanayaka@gmail.com>
2024-11-21 20:05:21 +0530

    Updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,10 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
-       <img src="{assets.logo}" alt="" />
+    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
+       <img className='w-44 cursor-pointer' src="{assets.logo}" alt="" />
        
-       <ul>
+       <ul className='hidden md:flex items-start gap-5 font-medium'>
         <NavLinkvLink>
           <li>Home</li>
           <hr />
