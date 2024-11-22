a806f7a318eace0e708c926c6618edfaa0d9a9de
tharindi22 <ttharindiattanayaka@gmail.com>
2024-11-22 12:38:32 +0530

    creating navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,25 +6,25 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
-       <img className='w-44 cursor-pointer' src="{assets.logo}" alt="" />
+       <img className='w-44 cursor-pointer' src={assets.logo} alt="" />
        
        <ul className='hidden md:flex items-start gap-5 font-medium'>
-        <NavLinkvLink>
+        <NavLink> 
           <li className='py-1'>Home</li>
           <hr className='boader-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
-        </NavLinkvLink>
-        <NavLinkvLink>
+        </NavLink>
+        <NavLink>
           <li className='py-1'>All Doctors</li>
           <hr className='boader-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
-        </NavLinkvLink>
-        <NavLinkvLink>
+        </NavLink>
+        <NavLink>
           <li className='py-1'>About</li>
           <hr className='boader-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
-        </NavLinkvLink>
-        <NavLinkvLink>
+        </NavLink>
+        <NavLink>
           <li className='py-1'>Contact</li>
           <hr className='boader-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
-        </NavLinkvLink>
+        </NavLink>
        </ul>
        <div>
         <button>Create account</button>
