275d7a20d354764e16dd0c08d6bd717f0b9a7205
tharindi22 <ttharindiattanayaka@gmail.com>
2024-11-24 23:01:54 +0530

    completed the Menu Section

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ const App = () => {
         <Route path='/appoinment/:docId' element={<Appoinment />} />
       </Routes>
 
-      <Header/>
+      
 
     </div>
     
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Header } from '../components/Header'
+import SpecialityMenu from '../components/SpecialityMenu'
 
 
 
@@ -7,6 +8,7 @@ const Home = () => {
   return (
     <div>
       <Header/>
+      <SpecialityMenu/>
     </div>
   )
 }
