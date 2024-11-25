9ed7b9628a502bcda3644aa4b2d44b047857f9d7
tharindi22 <ttharindiattanayaka@gmail.com>
2024-11-25 23:24:55 +0530

    completed the SpecialityMenu and Banner parts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Header } from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
+import TopDoctors from '../components/TopDoctors'
+import Banner from '../components/Banner'
 
 
 
@@ -9,6 +11,8 @@ const Home = () => {
     <div>
       <Header/>
       <SpecialityMenu/>
+      <TopDoctors/>
+      <Banner/>
     </div>
   )
 }
