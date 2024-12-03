2dcac411a2f059d99748f1988652c52e1c669d27
tharindi22 <ttharindiattanayaka@gmail.com>
2024-12-03 12:56:12 +0530

    updated files

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -22,6 +22,7 @@ const Banner = () => {
         <div className='hidden md:block md:w-1/2 lg:w-[370px] relative'>
             <img className='w-full absolute bottom-0 right-0 max-w-md' src={assets.appointment_img} alt="" />
         </div>
+        
     </div>
   )
 }
