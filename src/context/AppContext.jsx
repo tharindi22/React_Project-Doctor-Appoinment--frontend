53265ff7cc4ff1ef713152426d2073bec2c9a63c
tharindi22 <ttharindiattanayaka@gmail.com>
2025-01-30 21:45:12 +0530

    More Modifications for Appoinments

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,24 +1,29 @@
-import { createContext } from "react";
-import { doctors } from "../assets/assets";
+import { createContext, useState } from "react";
+import { doctors as initialDoctors } from "../assets/assets";
 
-export const AppContext =createContext()
+export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
+  const [doctors, setDoctors] = useState(initialDoctors); // Manage the state of doctors
 
-    const currencySymbol = '$'
+  const currencySymbol = "$";
 
+  // Function to cancel a specific doctor's appointment
+  const cancelAppointment = (_id) => {
+    setDoctors((prevDoctors) => prevDoctors.filter((doctor) => doctor._id !== _id));
+  };
 
-    const value ={
-        doctors,
-        currencySymbol
+  const value = {
+    doctors,
+    currencySymbol,
+    cancelAppointment, // Provide this method for components to use
+  };
 
-    }
+  return (
+    <AppContext.Provider value={value}>
+      {props.children}
+    </AppContext.Provider>
+  );
+};
 
-    return(
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
-    )
-}
-
-export default AppContextProvider
+export default AppContextProvider;
